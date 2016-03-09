a7abfd023839037f9d975582bbb51b1f85d270f3
Imanuel Ulbricht <i.ulbricht@codinglizards.de>
2016-03-09 23:59:35 +0100

    added user controller

---
+++ Coding.Lizards.Pokemon.Tools.Web/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿namespace Coding.Lizards.Pokemon.Tools.Web.Controllers {
+
+    using System.Web.Mvc;
+
+    public class UserController : Controller {
+
+        // GET: User
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
