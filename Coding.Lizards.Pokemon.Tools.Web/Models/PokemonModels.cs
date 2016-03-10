631a3377aa7413ee5deace977a491c176e3aef63
Imanuel Ulbricht <i.ulbricht@codinglizards.de>
2016-03-10 21:12:51 +0100

    added pokemodels file

---
+++ Coding.Lizards.Pokemon.Tools.Web/Models/PokemonModels.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coding.Lizards.Pokemon.Tools.Web.Models {
+    public class PokemonModels {
+    }
+}
